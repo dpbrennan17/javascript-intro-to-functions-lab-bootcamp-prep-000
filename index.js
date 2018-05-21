e4f9267821f1cb6052d66993294f9718d3af26d4
David Brennan <d.p.brennan@tcu.edu>
2018-05-21 01:18:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toUpperCase() === string
+  if ( string.toUpperCase() === string )
   return "YES INDEED!";
 }
 
 function sayHiToGrandma(string) {
-  string === string.toLowerCase()
+  if ( string === string.toLowerCase() )
   return "I can't hear you!";
 }
 
 function sayHiToGrandma(string) {
-  string === 'I love you, Grandma.'
+  if ( string === 'I love you, Grandma.' )
   return "I love you, too.";
 }
